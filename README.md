c02f2e7322af982b22a0264594f397f8174f615e
Ziroll <jsparlingzi3757@gmail.com>
2024-04-18 18:18:27 -0500

    Added starter code, then modified javascript to meet acceptance critera.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # PayRoll-Repo-Jerry-Sparling
+
+## Description
+
+This is an Employee Payroll tracker that utilizes JavaScript to calculate average salaries and choose random employees to win a drawing. The drawing winners and the average salary is displayed inside the console. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Used to keep track of employee's names and salaries. Displays average salary, and randomly selects an employee.
+
+## Credits
+
+Starter code for this project done by https://github.com/Xandromus 
+
+## License
+
+MIT License
+
+Copyright (c) 2024 Jrsparling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ assests/script.js
@@ -0,0 +1,113 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+
+    // TODO: Get user input to create and return an array of employee objects
+const collectEmployees = function() {
+  const employeeData=[]
+//Given a prompt, "ok" continues to next selection, "cancel" cancels the prompt
+let again = true
+while(again){
+const nameEmployeesFirst= prompt("Enter First Name");
+const nameEmployeesLast= prompt("Enter Last Name");
+let nameEmployeesSalary= prompt("Enter Salary");
+if (isNaN (nameEmployeesSalary)){
+  nameEmployeesSalary = 0
+}
+const newEmployee = {
+  firstName: nameEmployeesFirst,
+  lastName: nameEmployeesLast,
+  salary: parseFloat (nameEmployeesSalary),
+}
+   employeeData.push(newEmployee)
+   again = window.confirm ("Would you like to add a new employee?")
+}
+
+  return employeeData
+}
+
+// TODO: Calculate and display the average salary
+const displayAverageSalary = function(employeesArray) {
+  let sum = 0
+    for (let i = 0; i < employeesArray.length; i++){
+      let employeedrawing = employeesArray[i]
+      let employeessalary = employeedrawing.salary
+      sum += employeessalary
+    }
+    console.log(sum/employeesArray.length)  
+}
+
+  // TODO: Select and display a random employee
+const getRandomEmployee = function(employeesArray) {
+let randomNumber = Math.floor( Math.random() *employeesArray.length);
+console.log(employeesArray[randomNumber])
+let drawingWinner = ["Congrats to", employeesArray[randomNumber].firstName , employeesArray[randomNumber].lastName, "on winning the drawing."].join(" ");
+console.log(drawingWinner)
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
